58bb7acdb86ad66b43e0ea9607ce52f1b8e3b45f
volches <peala@mail.ru>
2023-09-27 09:46:41 +0300

    Second commit

---
+++ src/js/innerPage.js
@@ -0,0 +1,9 @@
+
+import modal from './modules/modal.js';
+modal();
+
+import animationFunc from "./modules/animationFunc";
+animationFunc();
+
+import productFunc from './modules/productFunc.js';
+productFunc();
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const config = {
     mode: 'production',
     entry: {
         index: './src/js/index.js',
-        // contacts: './src/js/contacts.js',
+        menu: './src/js/menu.js',
+        innerPage: './src/js/innerPage.js'
         // about: './src/js/about.js',
     },
     output: {
